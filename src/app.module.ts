9f1bb391e08825c0422526cbc6fc2cfddd674d05
Bernardo Domingues <bernardoopd@hotmail.com>
2024-04-06 10:05:17 -0300

    fix: nodemailer error and transporter sintax

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,16 +22,14 @@ import { MailerModule } from '@nestjs-modules/mailer';
       synchronize: true,
       migrationsTableName: 'typeorm_migrations',
     }),
-    MailerModule.forRootAsync({
-      useFactory: () => ({
-        transport: {
-          service: 'gmail',
-          auth: {
-            user: process.env.MAIL_USER,
-            pass: process.env.MAIL_PASS,
-          },
+    MailerModule.forRoot({
+      transport: {
+        service: 'gmail',
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS,
         },
-      }),
+      },
     }),
     UserModule,
     ExpenseModule,
